7466b0f9cd6c7ba9f6fd0623dad93745bc1ba08c
GilTorch <infotorch2014@gmail.com>
2026-09-03 08:22:29 -0400

    feat: implement task management with dynamic user tasks and fallback UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from "./tasks/tasks.component";
 import { User } from './user/user';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',  
   styleUrl: './app.component.css',
 })
